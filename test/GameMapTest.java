1e8e71282571e60b6cd51215a2de734c2de6aed5
JamesShaklee <126708035+JamesShaklee@users.noreply.github.com>
2025-05-20 13:33:50 -0400

    Attempting to make tests

--- test/GameMapTest.java
+++ test/GameMapTest.java
@@ -2,3 +2,25 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.Map;
+
+
+public class GameMapTest {
+    private GameMap gameMap;
+    private Map testMap;
+
+    @Before
+    public void setUp() throws Exception{
+        gameMap = new GameMap();
+    }
+
+    @Test
+    public void testGameMapGetMap() throws Exception{
+        testMap = gameMap.getMap();
+        Assert.assertEquals(testMap, gameMap.getMap());
+
+    }
+
+
+}
